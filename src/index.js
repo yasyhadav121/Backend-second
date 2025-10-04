5fafb17aba85122a06891c4af7764f79ad40e90e
Yash Yadav <yashyadav2003121@gamil.com>
2025-10-05 04:14:57 +0530

    h

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 
 // CORS Configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://front-end-one-puce-15.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
