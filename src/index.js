c8c87d4f73bc3a84329d1a89f1db100b215c2632
Yash Yadav <yashyadav2003121@gamil.com>
2025-10-05 10:57:07 +0530

    try

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 
 // CORS Configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://front-end-nine-delta-35.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
